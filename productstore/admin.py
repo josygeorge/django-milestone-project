333efaf50021139bae9a68820307b544dd62b52d
josygeorge <joe.george.josy@gmail.com>
2021-08-03 23:15:36 +0000

    Product Admin design

--- productstore/admin.py
+++ productstore/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_name',
+        'price',
+        'stock',
+        'category',
+        'modified_at',
+        'is_available')
+
+
+admin.site.register(Product)
