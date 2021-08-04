9e1c217d5d5061ecec3f6481598de87dd1c7aa7f
josygeorge <joe.george.josy@gmail.com>
2021-08-04 10:26:46 +0000

    Product detail done and certain bug fixes

--- productstore/models.py
+++ productstore/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from category.models import Category
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,7 +9,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=400, blank=True)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     images = models.ImageField(
         upload_to='photos/products', blank=True)
     stock = models.IntegerField()
@@ -16,6 +17,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
---
+++ productstore/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name='products'),
+    path(
+        '<slug:category_slug>/',
+        views.all_products,
+        name='products_by_category'),
+    path(
+        '<slug:category_slug>/<slug:product_slug>/',
+        views.product_detail,
+        name='product_detail')
+]
