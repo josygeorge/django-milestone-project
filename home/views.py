11c12bb78ee8a3311d940af90c828f68407b719b
josygeorge <joe.george.josy@gmail.com>
2021-08-04 04:19:13 +0000

    Products display logic

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from productstore.models import Product
 
 # Create your views here.
 
+
 def index(request):
     """ A view to return the index page """
+    products = Product.objects.all().filter(is_available=True)
 
-    return render(request, 'home/index.html')
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home/index.html', context)
